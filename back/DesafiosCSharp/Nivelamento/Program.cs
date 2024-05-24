c1f632e098f868074ff6971cfaf6b67f28ec0402
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2024-05-24 16:21:19 -0300

    projeto piramide (1) finalizado

---
+++ back/DesafiosCSharp/Nivelamento/Program.cs
@@ -0,0 +1,13 @@
+﻿using Nivelamento.models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            var piramide = new Piramide(1);
+            Console.WriteLine(piramide.Desenha());
+        } catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+    }
+}
---
+++ back/DesafiosCSharp/Nivelamento/models/Piramide.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivelamento.models
+{
+    internal class Piramide(int n)
+    {
+        public int N {  get; set; } = n >= 1 ? n : throw new Exception("n must be greater or iqual than 1");
+
+        public string Desenha()
+        {   
+            string piramide = "";
+            for (int i = 1; i <= N; i++)
+            {
+                if(i < n){
+                    piramide += $"{Piramide.AddSpace(n - i)}{generateRow(i)}\n";
+                }
+                else { piramide += $"{Piramide.AddSpace(n - i)}{generateRow(i)}"; }
+            }
+                
+                return piramide;
+        }
+
+        private static string AddSpace(int howManyTimes)
+        {
+            return string.Concat(Enumerable.Repeat(' ', howManyTimes));
+        }
+
+        private string generateRow(int n) {
+            if (n == 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                string row = string.Empty ;
+                for (int i = 1; i <= n; i++)
+                {
+                    row += i;
+                }
+
+                for (int i = n - 1; i >= 1; i--)
+                {
+                    row += i;
+                }
+                return row;
+            }
+        }
+    }
+}
