b42cdd5f6af9de9c1181ecfe68ad52e3efff0224
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2024-05-27 10:54:27 -0300

    exercicio 2 (vertice) finalizado

---
+++ back/DesafiosCSharp/Vertice/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Vertice
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Vertice v1 = new(3.0f, 4.0f);
+            Vertice v2 = new(3.0f, 4.0f);
+            Vertice v3 = new(7.0f, 1.0f);
+
+            Console.WriteLine($"Posição inicial: ({v1.X}, {v1.Y})");
+
+            float distancia = v1.CalcularDistancia(v3);
+            Console.WriteLine($"A distância em relação ao vértice ({v3.X}, {v3.Y}) é: {distancia}");
+
+            v1.Move(7.0f, 1.0f);
+            Console.WriteLine($"Nova posição: ({v1.X}, {v1.Y})");
+
+            Console.WriteLine($"v1 é igual a v2? {v1.Equals(v2)}");
+            Console.WriteLine($"v1 é igual a v3? {v1.Equals(v3)}");
+        }
+    }
+}
---
+++ back/DesafiosCSharp/Vertice/Vertice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vertice
+{
+    internal class Vertice(float X, float Y)
+    {
+        public float X { get; private set; } = X;
+        public float Y { get; private set; } = Y;
+
+        public float CalcularDistancia(Vertice outroVertice)
+        {
+            float deltaX = this.X - outroVertice.X;
+            float deltaY = this.Y - outroVertice.Y;
+            return (float)Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+        public void Move(float novoX, float novoY)
+        {
+            this.X = novoX;
+            this.Y = novoY;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Vertice vertice &&
+                   X == vertice.X &&
+                   Y == vertice.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+    }
+}
