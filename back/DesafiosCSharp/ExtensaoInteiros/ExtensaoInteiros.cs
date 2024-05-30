3622af11ff73aeff96aadb2ad5ff6a59beadf231
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2024-05-30 20:29:20 -0300

    Exercicio7 finalizado (armstrong numbers)

---
+++ back/DesafiosCSharp/ExtensaoInteiros/ExtensaoInteiros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong
+{
+    public static class ExtensaoInteiros
+    {
+        public static bool IsArmstrong(this int n)
+        {
+            string numeroString = n.ToString();
+            int digitosQuantidade = numeroString.Length;
+
+            int sum = numeroString.Select(digit => (int)Math.Pow(char.GetNumericValue(digit), digitosQuantidade)).Sum();
+
+            return sum == n;
+        }
+    }
+}
