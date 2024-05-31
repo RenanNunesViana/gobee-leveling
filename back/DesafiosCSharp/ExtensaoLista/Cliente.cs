6d92d91d54e26d24f0331472255f32c53a1af58f
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2024-05-31 12:54:16 -0300

    Exercicio 10 finalizado (extensao lista)

---
+++ back/DesafiosCSharp/ExtensaoLista/Cliente.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ExtensaoLista
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+
+        public Cliente(string nome, string cpf)
+        {
+            Nome = nome;
+            CPF = cpf;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Cliente cliente &&
+                   CPF == cliente.CPF;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CPF);
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}\nCPF: {CPF}";
+        }
+    }
+}
